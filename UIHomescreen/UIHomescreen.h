2f9707e6b119f5abf84a3b35b9b4c43390bd7696
Soheil <ysoheil@gmail.com>
2013-10-09 16:59:24 -0700

    create blank static lib project

---
+++ UIHomescreen/UIHomescreen.h
@@ -0,0 +1,13 @@
+//
+//  UIHomescreen.h
+//  UIHomescreen
+//
+//  Created by Soheil Yasrebi on 10/9/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIHomescreen : NSObject
+
+@end
---
+++ UIHomescreen/UIHomescreen.m
@@ -0,0 +1,13 @@
+//
+//  UIHomescreen.m
+//  UIHomescreen
+//
+//  Created by Soheil Yasrebi on 10/9/13.
+//
+//
+
+#import "UIHomescreen.h"
+
+@implementation UIHomescreen
+
+@end
